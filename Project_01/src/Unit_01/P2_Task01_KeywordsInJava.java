5db5605b82dd94396641cad174cad97974fc54f9
mihir <mihir@DESKTOP-NV6OI56>
2022-03-31 03:44:40 +0530

    Commit 2

---
+++ Project_01/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,22 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsInJava {
+	public static void main(String[] args) {
+		
+//		Syntax error on token "int", invalid VariableDeclaratorId
+		double int = 10;
+		
+//		Syntax error on token "int", delete this token
+		P2_Task01_KeywordsInJava int = new P2_Task01_KeywordsInJava();
+		
+	}
+//	Syntax error on token "double", Identifier expected
+	void double() {
+		
+	}
+	
+//	Syntax error on token "int", Identifier expected
+	class int() {
+		
+	}
+}
