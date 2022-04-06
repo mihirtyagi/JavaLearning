d5fe3fa859c7d26146d35007e976c842dd628218
mihir <mihir@DESKTOP-NV6OI56>
2022-04-07 03:43:57 +0530

    ..

--- Project_01/src/Unit_01/P4_Task02_WrapperClassesInJava.java
+++ Project_01/src/Unit_01/P4_Task02_WrapperClassesInJava.java
@@ -3,8 +3,23 @@ package Unit_01;
 public class P4_Task02_WrapperClassesInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		// Converting int into Integer
+		int a= 20;
+		
+		String s = "" + a +"";
+		
+		Integer i = Integer.valueOf(a); //Converting int into interger explicitly
+		Integer j = a; //autoboxing, now compiler will write Integer.valueOf(a) Internally
+		
+		System.out.println(a + " " + " " j);
+		
+		System.out.println(byteobj);
+		
+		//Unboxoing: Converting Objexts to Primitives
+		byte bytevalue = byteobj;
+		System.out.println(bytevalue);
+		
+		}
 
 	}
-
-}
--- Project_01/src/Unit_01/P5_Task02_MethodsInJava.java
+++ Project_01/src/Unit_01/P5_Task02_MethodsInJava.java
@@ -1,10 +1,31 @@
 package Unit_01;
 
 public class P5_Task02_MethodsInJava {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ABC3 obj = new ABC3();
+		obj.display();
+		System.out.println(obj.a);	
+		}
 
+}
+
+class ABC3 {
+	static int a = 10;
+	
+	static void display() {
+		int b = 10;
+		int a = 10;
+		System. out.println(b);
+
+		 System.out.println(a);
 	}
+		int display2() {
+		System.out.println(a);
+		//As a was a local variable so we can not access it outside the method
+		//System.out.printin(b);
+		return a;
 
+		}
+	
 }
