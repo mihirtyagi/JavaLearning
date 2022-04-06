2fbdb9ac77815195378029bc648c8861afced73c
mihir <mihir@192.168.1.44>
2022-04-06 10:19:53 +0530

    5th task

---
+++ Project_01/src/Unit_01/P4_Task02_WrapperClassesInJava.java
@@ -0,0 +1,10 @@
+package Unit_01;
+
+public class P4_Task02_WrapperClassesInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project_01/src/Unit_01/P4_Task03_ScannerClassInJava.java
@@ -0,0 +1,10 @@
+package Unit_01;
+
+public class P4_Task03_ScannerClassInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
