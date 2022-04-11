bddc4a775d9df0bf5b63044a61382a44446787a5
mihir <mihir@192.168.1.44>
2022-04-11 08:36:02 +0530

    yv

--- Project_01/src/Unit_01/P2_Task01_KeywordsInJava.java
+++ Project_01/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -3,7 +3,7 @@ package Unit_01;
 public class P2_Task01_KeywordsInJava {
 	public static void main(String[] args) {
 		
-//		Syntax error on token "int", invalid VariableDeclaratorI
+//		Syntax error on token "int", invalid VariableDeclaratorId
 		double int = 10;
 		
 //		Syntax error on token "int", delete this token
