a3b4806c2dc53659a5b07c202713b9cf08dac6a4
mihir <mihir@192.168.1.35>
2022-05-15 04:38:15 +0530

    Assignment 1

---
+++ Project_01/src/Unit_02/PracticeQues.java
@@ -0,0 +1,17 @@
+package Unit_02;
+
+import java.util.*;
+/*
+ * WAP which contain 6 objects of a class student Student(name,age,section,percentage)
+ * They all belong to Section A. Your program would be able to find the average
+ * percentage in this section. Use constructor to create these 6 objects and input from
+ * scanner class. 
+ */
+public class PracticeQues {
+	public static void main(String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
